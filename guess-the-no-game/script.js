383e6342732a7b36cfddc9726730593cbaa3424a
Rihika <116342520+rihikajain@users.noreply.github.com>
2025-07-22 15:20:23 +0530

    updates

--- guess-the-no-game/script.js
+++ guess-the-no-game/script.js
@@ -1,44 +1,27 @@
 'use strict';
-console.log(document.querySelector('.message').textContent)
-
+// console.log(document.querySelector('.message').textContent)
+function msg(input) {
+  document.querySelector('.message').textContent = input
+}
 let random = Math.trunc(Math.random() * 10) + 1
 
 let score = 10
 let highscore = 0
+
+
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value)
   console.log(guess)
 
   if (!guess) {
-    document.querySelector('.message').textContent = "⛔ Input the Number!"
+    msg("⛔ Input the Number!")
   }
-  else if (guess > random) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = "📈 High, Try Again!"
-      score--
-      document.querySelector('.score').textContent = score
-    }
-    else {
-      document.querySelector('.message').textContent = "😶 You Lost the Game! Play Again!"
-    }
-  }
-
-  else if (guess < random && guess > 0) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = "📉 Low, Try Again!"
-      score--
-      document.querySelector('.score').textContent = score
-
-    }
-    else {
-      document.querySelector('.message').textContent = `😶 You Lost the Game! 
-      Play Again!`
-    }
-  }
-
+  // else if (guess > 11 && guess < 0) {
+  //   document.querySelector('.message').textContent = "‼️Invalid Number! Range is from 1 to 10"
+  // }
   else if (guess === random) {
-    document.querySelector('.message').textContent = "✅ Correct!"
+    msg("✅ Correct!  You Won")
     if (score > highscore) {
       highscore = score
     }
@@ -46,12 +29,29 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.highscore').textContent = highscore
     document.querySelector('body').style.backgroundColor = '#60b347'
     document.querySelector('.number').style.width = '30rem'
+  }
 
+
+  else if (guess !== random) {
+    if (score > 1) {
+      msg((guess < random) ? "📉 Low, Try Again!" : "📈 High, Try Again!")
+      score--
+      document.querySelector('.score').textContent = score
+    }
+    else {
+      document.querySelector('.message').textContent = "😶 You Lost the Game! Play Again!"
+      document.querySelector('.score').textContent = 0
+      // document.querySelector('.number').textContent = random
+      document.querySelector('body').style.backgroundColor = '#b7554cff'
+      document.querySelector('.number').style.width = '30rem'
+
+
+    }
   }
 })
 
 document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('.message').textContent = "Start guessing..."
+  msg("Start guessing...")
   score = 10
   document.querySelector('.score').textContent = score
   random = Math.trunc(Math.random() * 10) + 1
