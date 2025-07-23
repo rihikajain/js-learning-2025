507ac4a940006cab54527f9ffea24ba6b7cf94a4
Rihika <116342520+rihikajain@users.noreply.github.com>
2025-07-23 13:00:16 +0530

    updates

--- dice-game/script.js
+++ dice-game/script.js
@@ -10,51 +10,77 @@ const neww = document.querySelector('.btn--new')
 const diceel = document.querySelector('.dice')
 const curr0 = document.querySelector('#current--0')
 const curr1 = document.querySelector('#current--1')
-let currrentscore = 0
-let activePlayer = 0
-let scores = [0, 0]
+let currrentscore, activePlayer, scores, playing
 
+function init() {
+  currrentscore = 0
+  activePlayer = 0
+  scores = [0, 0]
+  playing = true
 
-score0el.textContent = 0
-score1el.textContent = 0
-// console.log(score0el.textContent, score1el.textContent)
+  score0el.textContent = 0
+  score1el.textContent = 0
 
-diceel.classList.add("hidden")
+  curr0.textContent = 0
+  curr1.textContent = 0
+
+  diceel.classList.add("hidden")
+  document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner')
+  document.querySelector(`.player--0`).classList.add('player--active')
+
+}
+init()
+const switchh = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0
+  currrentscore = 0
+  activePlayer = activePlayer === 0 ? 1 : 0
+  player0.classList.toggle('player--active')
+  player1.classList.toggle('player--active')
+}
 
 roll.addEventListener('click', function () {
-  const randomdice = Math.trunc(Math.random() * 6) + 1
-  diceel.classList.remove("hidden")
-  console.log(randomdice)
-  diceel.src = `dice-${randomdice}.png`
+  if (playing === true) {
+    const randomdice = Math.trunc(Math.random() * 6) + 1
+    diceel.classList.remove("hidden")
+    console.log(randomdice)
+    diceel.src = `dice-${randomdice}.png`
 
-  // let active = document.getElementsByName(`player--${activePlayer}`)
+    if (randomdice !== 1) {
+      currrentscore += randomdice
+      document.getElementById(`current--${activePlayer}`).textContent = currrentscore
+    }
 
-  // doubt?????
+    else {
+      switchh()
+      // doubt?????
+      // let active = document.getElementsByName(`player--${activePlayer}`)
 
-  if (randomdice !== 1) {
-    currrentscore += randomdice
-    document.getElementById(`current--${activePlayer}`).textContent = currrentscore
-  }
+      // why cant we do like this, rendering active palyer by class name and then similarly the way we updataed the current score, befroe chnaging the activestate, remove class active from previous active and then add the class to new active???????
 
-  else {
+      // active.classList.add('player--active')
+      // active.classList.remove('player--active')
 
-    document.getElementById(`current--${activePlayer}`).textContent = 0
-    currrentscore = 0
-    activePlayer = activePlayer === 0 ? 1 : 0
-
-    // why cant we do like this, rendering active palyer by class name and then similarly the way we updataed the current score, befroe chnaging the activestate, remove class active from previous active and then add the class to new active???????
-
-    // active.classList.add('player--active')
-    // active.classList.remove('player--active')
-
-
-    player0.classList.toggle('player--active')
-    player1.classList.toggle('player--active')
-
+    }
   }
 })
-
 
 hold.addEventListener('click', function () {
+  if (playing === true) {
 
+    scores[activePlayer] += currrentscore
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
+    console.log(`Player ${activePlayer} updated score :`, scores[activePlayer])
+    if (scores[activePlayer] >= 20) {
+      console.log(`Player ${activePlayer} is the winner with a score of`, scores[activePlayer])
+
+      diceel.classList.add("hidden")
+      playing = false
+      document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+      document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+    }
+    else { switchh() }
+
+  }
 })
+
+neww.addEventListener('click', init)
